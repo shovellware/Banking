1c97a88567f11128745e9a066fc08721df14e2e7
unknown <shovelware@protonmail.ch>
2019-03-08 20:48:39 -0800

    first commit

---
+++ Banking/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: ask for login info, PIN number
+
+            // stand-in object with pre-defined account balance
+            User dom = new User("shovelware")
+            {
+                Checking = 5000,
+                Savings = 2000
+            };
+
+            // this loop is designed to make 1 transaction at a time, like an ATM
+            // at the end, you are given the option to make another transaction, or quit
+            bool deciding = true;
+            do
+            {
+                Console.Clear();
+                dom.ChooseAccount();
+                MainMenu();
+
+                
+                int mainMenuChoice = int.Parse(Console.ReadLine());
+
+                decimal transactionAmount = 0;
+                // the program only needs to know the amount being put in or taken out if 
+                // a deposit or withdrawal action is being taken
+                if (mainMenuChoice == 1 || mainMenuChoice == 2)
+                {
+                    Console.WriteLine("How much: ");
+                    transactionAmount = decimal.Parse(Console.ReadLine());
+                }
+
+                switch (mainMenuChoice)
+                {
+                    case 1:
+                        dom.MakeDeposit(transactionAmount);
+                        break;
+                    case 2:
+                        dom.MakeWithdrawal(transactionAmount);
+                        break;
+                    case 3:
+                        dom.GetStatus();
+                        break;
+                    case 4:
+                        deciding = false;
+                        break;
+                }
+
+                Console.Write("Would you like to make another transaction? y/n \n>");
+                string startOver = Console.ReadLine();
+
+                if (startOver.ToLower().Equals("n"))
+                    deciding = false;
+
+
+            } while (deciding);
+        }
+
+
+        public static void MainMenu()
+        {
+            Console.WriteLine("\nWhat would you like to do?");
+            Console.WriteLine(" 1 - Deposit \n" +
+                              " 2 - Withdrawal \n" +
+                              " 3 - Check Balance \n" +
+                              " 4 - Quit");
+        }
+
+        //TODO: keep track of all transactions
+
+
+        //tryparse method needs to be added here
+    }
+}
---
+++ Banking/User.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class User
+    {
+        enum AccountType { checking = 1, savings }
+
+        private string _username;
+        private decimal _checking;
+        private decimal _savings;
+        private int _account;
+
+        public User(string username)
+        {
+            _username = username;
+            Console.WriteLine("Hello " + _username);
+            Console.WriteLine("---------------------");
+        }
+
+
+        public decimal Checking
+        {
+            get { return _checking; }
+            set { _checking = value; }
+        }
+
+        public decimal Savings
+        {
+            get { return _savings; }
+            set { _savings = value; }
+        }
+
+        public void MakeDeposit(decimal depAmount)
+        {
+            switch (_account)
+            {
+                case (int)AccountType.checking:
+                    _checking += depAmount;
+                    break;
+                case (int)AccountType.savings:
+                    _savings += depAmount;
+                    break;
+            }
+        }
+
+        //TODO: can't take out more money than you have! add these conditions
+        public void MakeWithdrawal(decimal withAmount)
+        {
+            switch(_account)
+            {
+                case (int)AccountType.checking:
+                    _checking -= withAmount;
+                    break;
+                case (int)AccountType.savings:
+                    _savings -= withAmount;
+                    break;
+            }
+        }
+
+        // I need to know which account the user is working with (checking or savings)
+        // this method is called in the User() constructor, at the top of this class...
+        // Also testing my usage of enum and how necessary it would be in this case,
+        // when AccountType is always associated with 1 and 2
+        public void ChooseAccount()
+        {
+            Console.WriteLine("Choose an account:\n " +
+                "1 - Checking\n " +
+                "2 - Savings");
+
+            int choice;
+            //loop until a valid selection is found
+            bool invalid = true;
+            do
+            {
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        _account = (int)AccountType.checking;
+                        invalid = false;
+                        break;
+                    case 2:
+                        _account = (int)AccountType.savings;
+                        invalid = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection");
+                        break;
+                }
+            } while (invalid);
+        }
+
+
+        public void GetStatus()
+        {
+            Console.WriteLine("Checking Balance: {0:C2} \nSavings Balance: {1:C2}\n", _checking, _savings);
+        }
+    }
+}
